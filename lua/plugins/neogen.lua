7019bdb0ba69b9389a0dca250c93b09c80b8add3
Juan <juan.ochoa@moonlightcompanies.com>
2024-01-25 00:16:37 -0800

    Call neogen setup in config file to have neovim recognize Neogen commands.

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -11,6 +11,8 @@ return {
 
   -- config = true,
   config = function ()
+      require('neogen').setup()
+
       -- FIXME: 23/1204 (luasnip loading errors)
       -- snippet_engine = "luasnip"
 
@@ -23,6 +25,7 @@ return {
       -- "   <leader>nm: file/module
       -- "-----------------------------------"
       local opts = { noremap = true, silent = true }
+
       vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
       vim.api.nvim_set_keymap("n", "<Leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
       vim.api.nvim_set_keymap("n", "<Leader>nm", ":lua require('neogen').generate({ type = 'file' })<CR>", opts)
