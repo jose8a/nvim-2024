21e5b492c8c473a60077a9db81037c6cf219741a
Juan <juan.ochoa@moonlightcompanies.com>
2025-11-07 04:56:26 -0800

    Update status bar. Add a winbar along top of file.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -46,14 +46,23 @@ return {
         -- section_separators = '',
       },
       sections = {
+        lualine_a = { "mode" },
+        lualine_b = {},
         lualine_c = { "filename" },
         lualine_x = {},
-        -- lualine_y = {},
-        lualine_y = { "diagnostics" },
+        lualine_y = {},
         lualine_z = { "progress"},
       },
       inactive_sections = {
-        -- lualine_z = {},
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = { "branch", "diff" },
+        lualine_c = {},
+        lualine_x = {},
+        -- lualine_y = {},
+        lualine_y = { "diagnostics" },
+        lualine_z = {},
       }
     },
   },
